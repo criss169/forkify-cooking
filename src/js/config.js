56e0056b5ba0be3fc14146715765df5308f6b6de
criss169 <cristina_aftode@yahoo.com>
2024-04-18 13:39:21 +0300

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,14 @@
+export const API_URL = "https://forkify-api.herokuapp.com/api/v2/recipes/";
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = "39d5896a-919d-48f1-b6c5-010093389da2";
+export const MODAL_CLOSE_SEC = 2.5;
+
+// Improvement and feature ideas: Challenges
+// Display number of pages between the pagination buttons
+// Ability to sort search results by duration or number of ingredients
+// Improve recipe ingredient input: separate in multiple fields and allow more than 6 ingredients;
+
+// Shopping list feature: button on recipe to add ingredients to a list
+// Weekly meal planning feature: assign recipes to the next 7 days and show on a weekly calendar
+// Get nutrition data on each ingredient from spoonacular API and calculate total calories of recipe.(https://spoonacular.com/food-api)
